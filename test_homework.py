6506f4dcfff7ecda4bb914caab4e0741b836e405
Sergey Ikonnikov <s.ikonnikov@profeelab.net>
2025-02-09 12:55:20 +0800

    reformat code

--- test_homework.py
+++ test_homework.py
@@ -1,6 +1,6 @@
-
 import pytest
 from selene import browser, be, have
+
 
 @pytest.fixture(scope="session")
 def browser_url():
@@ -8,14 +8,17 @@ def browser_url():
     yield
     browser.quit()
 
+
 @pytest.fixture(scope="session")
 def browser_res():
     browser.config.window_height = '2560'
     browser.config.window_width = '1440'
 
+
 def test_search(browser_url, browser_res):
     browser.element('[id="searchbox_input"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('html').should(have.text('yashaka/selene: User-oriented Web UI browser tests in Python'))
+
 
 def test_search_no_results(browser_url, browser_res):
     browser.element('[id="search_form_input"]').clear().type('атуцшоатуцшатушаиуцаицуш').press_enter()
